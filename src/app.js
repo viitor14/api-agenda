41cdffa6d39de66d6026a5472173921fbe3a0e75
viitor14 <manoelvitor550@gmail.com>
2024-11-25 19:27:59 -0300

    Adicionando o URL do servidor

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 
-const whiteList = ['http://localhost:3000', 'http://192.168.1.10:81', 'http://localhost:3001'];
+const whiteList = ['http://localhost:3000', 'api-agenda-lime.vercel.app', 'http://localhost:3001'];
 
 const corsOptions = {
   origin(origin, callback) {
