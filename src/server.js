135cfe82c5345f5516a52abfa2b1aaa4dc0eba8e
viitor14 <manoelvitor550@gmail.com>
2024-11-25 20:11:32 -0300

    att

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-// import app from './app';
+import app from './app';
 
-// const port = process.env.APP_PORT;
-// app.listen(port);
+const port = process.env.APP_PORT;
+app.listen(port);
 console.log('deu errado');
