4be768d85b6ce868063485b7de5eeb422139710b
viitor14 <manoelvitor550@gmail.com>
2024-11-25 20:24:38 -0300

    att

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,5 @@
-// import app from './app';
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-// const port = process.env.APP_PORT;
-// app.listen(port);
+const port = process.env.APP_PORT;
+_app2.default.listen(port);
 console.log('deu errado');
